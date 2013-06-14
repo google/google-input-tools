ced0b72038e05233c96f916b0ab35f90f6e622ac
Carbo Kuo <carbokuo@google.com>
2013-06-14 15:32:37 +0800

    Check in source code of nacl-hangul.

---
+++ src/chrome/os/nacl-hangul/js/keyboard.js
@@ -0,0 +1,164 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Keyboard layouts of Hangul IME.
+ */
+
+'use strict';
+
+/**
+ * Enum of Keyboard layout.
+ *
+ * @enum {string}
+ */
+HangulIme.Keyboard = {
+  DUBEOLSIK: 'hangul_2set',
+  SEBEOLSIK_390: 'hangul_3set390',
+  SEBEOLSIK_FINAL: 'hangul_3setfinal',
+  SEBEOLSIK_NOSHIFT: 'hangul_3setnoshift',
+  ROMAJA: 'hangul_romaja',
+  AHNMATAE: 'hangul_ahnmatae'
+};
+
+/**
+ * Layouts of Korean keyboards
+ *
+ * @type {Array}
+ */
+HangulIme.keyboardLayouts = [];
+
+HangulIme.keyboardLayouts.push({
+  id: HangulIme.Keyboard.DUBEOLSIK,
+  name: '2',
+  validKeys: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  charMap: {}
+});
+
+HangulIme.keyboardLayouts.push({
+  id: HangulIme.Keyboard.SEBEOLSIK_390,
+  name: '39',
+  validKeys: 'abcdefghijklmnopqrstuvwxyz1234567890!QWERASDFZXCV/;\'',
+  charMap: {
+    'T': ';',
+    'Y': '<',
+    'U': '7',
+    'I': '8',
+    'O': '9',
+    'P': '>',
+    'G': '/',
+    'H': '\'',
+    'J': '4',
+    'K': '5',
+    'L': '6',
+    'B': '!',
+    'N': '0',
+    'M': '1',
+    '<': '2',
+    '>': '3',
+  }
+});
+
+HangulIme.keyboardLayouts.push({
+  id: HangulIme.Keyboard.SEBEOLSIK_FINAL,
+  name: '3f',
+  validKeys: 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%QWERTASDFGZXCV/;\'',
+  charMap: {
+    '^': '=',
+    '&': '“',
+    '*': '”',
+    '(': '\'',
+    ')': '~',
+    '-': ')',
+    '=': '>',
+    '_': ';',
+    '+': '+',
+    'Y': '5',
+    'U': '6',
+    'I': '7',
+    'O': '8',
+    'P': '9',
+    '[': '(',
+    ']': '<',
+    '{': '%',
+    '}': '/',
+    '\\': ':',
+    '|': '\\',
+    'H': '0',
+    'J': '1',
+    'K': '2',
+    'L': '3',
+    ':': '4',
+    '"': '·',
+    'N': '-',
+    'M': '"',
+    '<': ',',
+    '>': '.',
+    '?': '!',
+  }
+});
+
+HangulIme.keyboardLayouts.push({
+  id: HangulIme.Keyboard.SEBEOLSIK_NOSHIFT,
+  name: '3s',
+  validKeys: 'abcdefghijklmnopqrstuvwxyz1234567890QWERASDFZXCV-=[]\\;\'/',
+  charMap: {
+    'T': ';',
+    'Y': '<',
+    'U': '7',
+    'I': '8',
+    'O': '9',
+    'P': '>',
+    'G': '/',
+    'H': '\'',
+    'J': '4',
+    'K': '5',
+    'L': '6',
+    'B': '!',
+    'N': '0',
+    'M': '1',
+    '<': '2',
+    '>': '3',
+  }
+});
+
+HangulIme.keyboardLayouts.push({
+  id: HangulIme.Keyboard.ROMAJA,
+  name: 'ro',
+  validKeys: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  charMap: {}
+});
+
+HangulIme.keyboardLayouts.push({
+  id: HangulIme.Keyboard.AHNMATAE,
+  name: 'ahn',
+  validKeys: 'abcdefghijklmnopqrstuvwxyzACDEFHKLMQRSVXZ;,./',
+  charMap: {
+    'W': '\u317f',
+    'T': '\u3186',
+    'Y': ';',
+    'U': '\'',
+    'I': '/',
+    'O': '[',
+    'P': ']',
+    '[': ',',
+    ']': '?',
+    'G': 'ㆁ',
+    'J': 'ㆍ',
+    '\'': '.',
+    'B': '\u11f0',
+    'N': '\u11eb',
+    '?': '\u11f9',
+  }
+});
---
+++ src/chrome/os/nacl-hangul/nacl/unicode_util.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef UNICODE_UTIL_H_
+#define UNICODE_UTIL_H_
+
+#include <string>
+
+using std::string;
+
+typedef uint32_t ucschar;
+typedef std::basic_string<ucschar> UCSString;
+
+namespace unicode_util {
+
+// Converts UCS4 string into UTF-8 format
+// @param[in] ucs4    The UCS4 string.
+// @param[in] length  The length of UCS4 string. If length is set to 0, ucs4
+//                    will be considered as a C-style string ending by 0.
+// @return            The converted UTF-8 string.
+string Ucs4ToUtf8(const ucschar* ucs4, size_t length);
+
+// Converts UTF-8 string into UCS4 format
+// @param[in] utf8    The UTF-8 string.
+// @param[in] length  The length of UTF-8 string. If length is set to 0, utf8
+//                    will be considered as a C-style string ending by 0.
+// @return            The converted UCS4 string.
+UCSString Utf8ToUcs4(const char* utf8, size_t length);
+
+}   // namespace unicode_util
+
+#endif  // UNICODE_UTIL_H_
