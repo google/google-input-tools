3027deb252264abcc485a27e79e5bc56741f3bba
Chen Gong <chengong@google.com>
2014-06-15 17:21:41 +0800

    Support MSVS 2013 by setting environment variable MSVS_VERSION.

--- src/client/gyp.bat
+++ src/client/gyp.bat
@@ -1,9 +1,10 @@
 @echo off
-set PROTOC=%~dp0\depends\\protoc.exe
+set PROTOC=%~dp0\depends\protoc.exe
 set PROTOBUF=%~dp0\depends\protobuf-2.5.0
 set GTEST=%~dp0\depends\gtest-1.7.0
 set ZLIB=%~dp0\depends\zlib-1.2.8
 set WTL80=%~dp0\depends\wtl80\
 set GYP=%~dp0\depends\gyp
-call %gyp%\gyp.bat --depth %~dp0 -I build\common.gypi -G msvs_version=2012
+if not defined MSVS_VERSION set MSVS_VERSION=2012
+call %gyp%\gyp.bat --depth %~dp0 -I build\common.gypi -G msvs_version=%MSVS_VERSION%
 python build_x86_x64_together.py
